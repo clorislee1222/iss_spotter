46aa8b51d0596912469dc865a55ca67006cf92e4
Cloris Li <cloris.lzy@gmail.com>
2023-02-20 09:44:39 -0800

    ISS Spotter II

--- index.js
+++ index.js
@@ -11,3 +11,13 @@ const { fetchMyIP } = require('./iss');
 //   console.log('It worked! Returned IP:' , ip);
 // });
 
+const { fetchCoordsByIP } = require('./iss');
+
+// fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned coordinates:' , coordinates);
+// });
